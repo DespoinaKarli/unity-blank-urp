79a888979902713710ef130161cdd378f235677d
DESPOINA KARLI <dkarli@edu.cdv.pl>
2023-03-16 15:28:38 +0100

    Funny comment

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        / //Hahahahah Funny Comm
     }
 
     // Update is called once per frame
